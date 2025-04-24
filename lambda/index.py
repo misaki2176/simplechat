2324721781f678ad4c0fc0c3ce095950ecc5cce7
misaki2176 <misaki.h.529@icloud.com>
2025-04-24 22:52:25 +0900

    URLのミスの修正

--- lambda/index.py
+++ lambda/index.py
@@ -4,7 +4,7 @@ import os
 import boto3
 import re  # 正規表現モジュールをインポート
 from botocore.exceptions import ClientError
-url='https://2ae0-34-125-223-137.ngrok-free.app/'
+url='https://2ae0-34-125-223-137.ngrok-free.app/generate'
 import urllib.request
 
 # Lambda コンテキストからリージョンを抽出する関数
